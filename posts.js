d2ddfc992c105d9d146591a11ebe1618e37016dd
Ayush Biswas <ayushbisswas@gmail.com>
2026-06-25 00:38:19 +0530

    Add files via upload
    redesigning of the website

---
+++ posts.js
@@ -0,0 +1,15 @@
+// Ayush Biswas | Journal Registry
+window.blogPosts = [
+    {
+        title: "The Digital Cleansing",
+        date: "June 24, 2026",
+        category: "Essays",
+        file: "posts/digital-cleansing.html"
+    },
+    {
+        title: "Building in Public",
+        date: "May 12, 2026",
+        category: "Updates",
+        file: "posts/building-in-public.html"
+    }
+];
